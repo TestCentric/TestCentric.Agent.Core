86fe08e2c067384f484a966076edf45d50577cd1
Charlie Poole <charliepoole@gmail.com>
2023-11-14 09:42:44 -0800

    Eliminate inheritance from AbstractTestRunner and simplify TestAgentRunner

--- src/TestCentric.Agent.Core/Runners/TestAgentRunner.cs
+++ src/TestCentric.Agent.Core/Runners/TestAgentRunner.cs
@@ -7,6 +7,7 @@ using System;
 using TestCentric.Engine.Drivers;
 using TestCentric.Engine.Internal;
 using TestCentric.Engine.Extensibility;
+using System.ComponentModel;
 
 namespace TestCentric.Engine.Runners
 {
@@ -15,7 +16,7 @@ namespace TestCentric.Engine.Runners
     /// deal directly with a framework driver. It loads and runs tests in a single
     /// assembly, creating an <see cref="IFrameworkDriver"/> to do so.
     /// </summary>
-    public abstract class TestAgentRunner : AbstractTestRunner
+    public abstract class TestAgentRunner : ITestEngineRunner
     {
         // TestAgentRunner loads and runs tests in a particular AppDomain using
         // one driver per assembly. All test assemblies are ultimately executed by
@@ -36,12 +37,14 @@ namespace TestCentric.Engine.Runners
         // Used to inject DriverService for testing
         internal IDriverService DriverService { get; set; }
 
-        public TestAgentRunner(TestPackage package) : base(package)
+        public TestAgentRunner(TestPackage package)
         {
             Guard.ArgumentNotNull(package, nameof(package));
             Guard.ArgumentValid(package.SubPackages.Count == 0, "Only one assembly may be loaded by an agent", nameof(package));
             Guard.ArgumentValid(package.FullName != null, "Package may not be anonymous", nameof(package));
             Guard.ArgumentValid(package.IsAssemblyPackage, "Must be an assembly package", nameof(package));
+
+            TestPackage = package;
 
             // Bypass the resolver if not in the default AppDomain. This prevents trying to use the resolver within
             // NUnit's own automated tests (in a test AppDomain) which does not make sense anyway.
@@ -53,32 +56,30 @@ namespace TestCentric.Engine.Runners
         }
 
         /// <summary>
-        /// Explores a previously loaded TestPackage and returns information
-        /// about the tests found.
+        /// The TestPackage, which this runner is handling
         /// </summary>
-        /// <param name="filter">The TestFilter to be used to select tests</param>
-        /// <returns>
-        /// A TestEngineResult.
-        /// </returns>
-        public override TestEngineResult Explore(TestFilter filter)
-        {
-            EnsurePackageIsLoaded();
-
-            try
-            {
-                return new TestEngineResult(_driver.Explore(filter.Text));
-            }
-            catch (Exception ex) when (!(ex is EngineException))
-            {
-                throw new EngineException("An exception occurred in the driver while exploring tests.", ex);
-            }
-        }
+        protected TestPackage TestPackage { get; set; }
 
         /// <summary>
-        /// Load a TestPackage for exploration or execution
+        /// The result of the last call to LoadPackage
+        /// for this runner's TestPackage.
+        /// </summary>
+        protected TestEngineResult LoadResult { get; set; }
+
+        /// <summary>
+        /// Indicates whether the runner's TestPackage is loaded.
+        /// </summary>
+        public bool IsPackageLoaded => LoadResult != null;
+
+        #region ITestEngineRunner Implementation
+
+        /// <summary>
+        /// Loads the TestPackage for exploration or execution,
+        /// saving the result. Overridden in derived classes to 
+        /// take any necessary action before or after loading.
         /// </summary>
         /// <returns>A TestEngineResult.</returns>
-        protected override TestEngineResult LoadPackage()
+        public virtual TestEngineResult Load()
         {
             var testFile = TestPackage.FullName;
             log.Info($"Loading package {testFile}");
@@ -102,15 +103,60 @@ namespace TestCentric.Engine.Runners
             _driver = DriverService.GetDriver(TestDomain, testFile, targetFramework, skipNonTestAssemblies);
             _driver.ID = TestPackage.ID;
 
-            log.Debug($"Using driver {_driver.GetType().Name}");      
-            
+            log.Debug($"Using driver {_driver.GetType().Name}");
+
             try
             {
-                return new TestEngineResult(_driver.Load(testFile, TestPackage.Settings));
+                return LoadResult = new TestEngineResult(_driver.Load(testFile, TestPackage.Settings));
             }
             catch (Exception ex) when (!(ex is EngineException))
             {
                 throw new EngineException("An exception occurred in the driver while loading tests.", ex);
+            }
+        }
+
+        /// <summary>
+        /// Reload the currently loaded test package, saving the result.
+        /// </summary>
+        /// <returns>A TestEngineResult.</returns>
+        /// <exception cref="InvalidOperationException">If no package has been loaded</exception>
+        public TestEngineResult Reload()
+        {
+            if (this.TestPackage == null)
+                throw new InvalidOperationException("MasterTestRunner: Reload called before Load");
+
+            // Currently, we just do a load
+            return Load();
+        }
+
+        /// <summary>
+        /// Unload any loaded TestPackage. Overridden in
+        /// derived classes to take any necessary action.
+        /// </summary>
+        public virtual void Unload()
+        {
+            LoadResult = null;
+        }
+
+        /// <summary>
+        /// Explores a previously loaded TestPackage and returns information
+        /// about the tests found.
+        /// </summary>
+        /// <param name="filter">The TestFilter to be used to select tests</param>
+        /// <returns>
+        /// A TestEngineResult.
+        /// </returns>
+        public TestEngineResult Explore(TestFilter filter)
+        {
+            EnsurePackageIsLoaded();
+
+            try
+            {
+                return new TestEngineResult(_driver.Explore(filter.Text));
+            }
+            catch (Exception ex) when (!(ex is EngineException))
+            {
+                throw new EngineException("An exception occurred in the driver while exploring tests.", ex);
             }
         }
 
@@ -120,7 +166,7 @@ namespace TestCentric.Engine.Runners
         /// </summary>
         /// <param name="filter">A TestFilter</param>
         /// <returns>The count of test cases</returns>
-        public override int CountTestCases(TestFilter filter)
+        public int CountTestCases(TestFilter filter)
         {
             EnsurePackageIsLoaded();
 
@@ -134,6 +180,70 @@ namespace TestCentric.Engine.Runners
             }
         }
 
+        /// <summary>
+        /// Run the tests in the TestPackage, loading the package
+        /// if this has not already been done.
+        /// </summary>
+        /// <param name="listener">An ITestEventHandler to receive events</param>
+        /// <param name="filter">A TestFilter used to select tests</param>
+        /// <returns>A TestEngineResult giving the result of the test execution</returns>
+        public TestEngineResult Run(ITestEventListener listener, TestFilter filter)
+        {
+            return RunTests(listener, filter);
+        }
+
+        /// <summary>
+        /// Start a run of the tests in the loaded TestPackage, returning immediately.
+        /// The tests are run asynchronously and the listener interface is notified
+        /// as it progresses.
+        /// </summary>
+        /// <param name="listener">An ITestEventHandler to receive events</param>
+        /// <param name="filter">A TestFilter used to select tests</param>
+        /// <returns>An <see cref="AsyncTestEngineResult"/> that will provide the result of the test execution</returns>
+        public AsyncTestEngineResult RunAsync(ITestEventListener listener, TestFilter filter)
+        {
+            var testRun = new AsyncTestEngineResult();
+
+            using (var worker = new BackgroundWorker())
+            {
+                worker.DoWork += (s, ea) =>
+                {
+                    var result = RunTests(listener, filter);
+                    testRun.SetResult(result);
+                };
+                worker.RunWorkerAsync();
+            }
+
+            return testRun;
+        }
+
+        /// <summary>
+        /// Cancel the ongoing test run. If no  test is running, the call is ignored.
+        /// </summary>
+        /// <param name="force">If true, cancel any ongoing test threads, otherwise wait for them to complete.</param>
+        public void RequestStop()
+        {
+            log.Info("Requesting stop");
+
+            EnsurePackageIsLoaded();
+
+            try
+            {
+                _driver.StopRun(false);
+            }
+            catch (Exception ex) when (!(ex is EngineException))
+            {
+                throw new EngineException("An exception occurred in the driver while stopping the run.", ex);
+            }
+        }
+
+        public void ForcedStop()
+        {
+            log.Info("Cancelling test run");
+            Environment.Exit(0);
+        }
+
+        #endregion
 
         /// <summary>
         /// Run the tests in the loaded TestPackage.
@@ -143,7 +253,7 @@ namespace TestCentric.Engine.Runners
         /// <returns>
         /// A TestEngineResult giving the result of the test execution
         /// </returns>
-        protected override TestEngineResult RunTests(ITestEventListener listener, TestFilter filter)
+        private TestEngineResult RunTests(ITestEventListener listener, TestFilter filter)
         {
             log.Info($"Running tests for {TestPackage.FullName}");
 
@@ -171,36 +281,29 @@ namespace TestCentric.Engine.Runners
             return new TestEngineResult(driverResult);
         }
 
-        /// <summary>
-        /// Cancel the ongoing test run. If no  test is running, the call is ignored.
-        /// </summary>
-        /// <param name="force">If true, cancel any ongoing test threads, otherwise wait for them to complete.</param>
-        public override void RequestStop()
-        {
-            log.Info("Requesting stop");
-
-            EnsurePackageIsLoaded();
-
-            try
-            {
-                _driver.StopRun(false);
-            }
-            catch (Exception ex) when (!(ex is EngineException))
-            {
-                throw new EngineException("An exception occurred in the driver while stopping the run.", ex);
-            }
-        }
-
-        public override void ForcedStop()
-        {
-            log.Info("Cancelling test run");
-            Environment.Exit(0);
-        }
-
         private void EnsurePackageIsLoaded()
         {
             if (!IsPackageLoaded)
-                LoadResult = LoadPackage();
+                Load();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected bool _disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                    Unload();
+
+                _disposed = true;
+            }
         }
     }
 }
