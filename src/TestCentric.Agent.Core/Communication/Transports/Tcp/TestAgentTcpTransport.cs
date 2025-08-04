2ee093ecab9cb6ef4292ef55aae10afcbe9ce84b
Charlie Poole <charliepoole@gmail.com>
2025-08-04 10:14:04 -0700

    Use TCP transport for all agents

--- src/TestCentric.Agent.Core/Communication/Transports/Tcp/TestAgentTcpTransport.cs
+++ src/TestCentric.Agent.Core/Communication/Transports/Tcp/TestAgentTcpTransport.cs
@@ -48,6 +48,7 @@ namespace TestCentric.Engine.Communication.Transports.Tcp
         {
             try
             {
+                System.Diagnostics.Debugger.Launch();
                 // Connect to the server
                 log.Debug($"Connecting to TestAgency at {_agencyUrl}");
                 _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
